81d1e74317e76081ccad281d7f871a3a36715c18
Renato Ghirelli <renato.ghirelli@gmail.com>
2025-02-03 21:48:59 -0300

    Encapsulamento Get e Set

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,32 +1,64 @@
+package br.com.alura.screenmatch.modelos;
+
 public class Filme {
 
-    String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
-    int duracaoEmMinutos;
+    private int duracaoEmMinutos;
 
-    int getTotalDeAvaliacoes() {
+
+    public String getNome() {
+        return nome;
+    }
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+    public double getSomaDasAvaliacoes() {
+        return somaDasAvaliacoes;
+    }
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+    public int getTotalDeAvaliacoes() {
         return totalDeAvaliacoes;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
     }
 
 
-    void exibeFichaTecnica(){
 
-        System.out.println("Nome do Filme: " + nome);
+
+    public void exibeFichaTecnica(){
+
+        System.out.println("Nome do br.com.alura.screenmatch.modelos.Filme: " + nome);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
 
     }
 
-    void avalia(double nota){
+    public void avalia(double nota){
 
         somaDasAvaliacoes += nota;
         totalDeAvaliacoes++;
 
     }
 
-    double pegaMedia(){
+    public double pegaMedia(){
 
         return somaDasAvaliacoes / totalDeAvaliacoes;
     }
